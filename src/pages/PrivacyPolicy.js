aea5a3a9cf611a4f3ebce15ca3168f92eff6d3dc
Oluwatobi Shokunbi <ayoshokz@gmail.com>
2025-05-17 13:51:23 +0100

    add title

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,10 +1,17 @@
 import ScreenContainer from "../components/screen";
 
 export default function PrivacyPolicy() {
-    return (
-      <ScreenContainer>
-        <h1>Privacy Policy</h1>
-      </ScreenContainer>
-    );
-  }
-  
+  return (
+    <ScreenContainer>
+      <section id="faq" className="py-16 bg-white">
+        <div className="max-w-4xl mx-auto px-4 md:px-6">
+          <h2 className="text-3xl font-semibold text-gray-800 text-center">
+            Privacy Policy
+          </h2>
+
+
+        </div>
+      </section>
+    </ScreenContainer>
+  );
+}
--- src/pages/TermsAndConditions.js
+++ src/pages/TermsAndConditions.js
@@ -3,7 +3,15 @@ import ScreenContainer from "../components/screen";
 export default function TermsAndConditions() {
   return (
     <ScreenContainer>
-        <h1>  Terms And Conditions</h1>
+      <section id="faq" className="py-16 bg-white">
+        <div className="max-w-4xl mx-auto px-4 md:px-6">
+          <h2 className="text-3xl font-semibold text-gray-800 text-center">
+            Terms And Conditions
+          </h2>
+
+
+        </div>
+      </section>
     </ScreenContainer>
   );
 }

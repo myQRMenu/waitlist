a217baffcb6b7f7fb695293131cbb0ce06e49fa9
Oluwatobi Shokunbi <ayoshokz@gmail.com>
2025-05-17 13:44:08 +0100

    add title

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -3,7 +3,7 @@ import ScreenContainer from "../components/screen";
 export default function PrivacyPolicy() {
     return (
       <ScreenContainer>
-        
+        <h1>Privacy Policy</h1>
       </ScreenContainer>
     );
   }
